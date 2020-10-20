62b3020676352dd7df5438865628c85df15dea0b
unknown <canhan97na@gmail.com>
2020-10-20 23:39:01 +0700

    update

--- SudokuBacktracker.m
+++ SudokuBacktracker.m
@@ -27,32 +27,39 @@ end
 
 function [ flag ] = ConflictTest( A,row_i,col_j, x )
 flag = 0;
+A(row_i, col_j) = x;
 %% Column test
+demcol = zeros(9, 1);
 for ii = 1:9
-   if ii~=row_i
-      if A(ii,col_j) == x
-          return
-      end
+   if A(ii, col_j) ~= 0
+       demcol(A(ii, col_j), 1) = demcol(A(ii, col_j), 1) + 1;
+       if demcol(A(ii, col_j), 1) > 1
+           return
+       end
    end
 end
 
 %% Row test
+demrow = zeros(1, 9);
 for jj = 1:9
-    if jj~=col_j
-        if A(row_i,jj) == x
+    if A(row_i,jj) ~= 0
+        demrow(1, A(row_i,jj)) = demrow(1, A(row_i,jj)) + 1;
+        if demrow(1, A(row_i,jj)) > 1
            return 
         end
     end
 end
 %% 3x3 grid tests
+dem = zeros(1, 9);
 modRow = mod(row_i-1,3);
 modCol = mod(col_j-1,3);
 row_l  = row_i - modRow;
 col_l  = col_j - modCol;
 for ii = row_l:row_l+2
     for jj = col_l:col_l+2
-        if(ii~=row_i) || (jj~=col_j)
-           if A(ii,jj) == x
+        if A(ii,jj) ~= 0
+            dem(1, A(ii, jj)) = dem(1, A(ii, jj)) + 1;
+           if dem(1, A(ii, jj)) > 1
               return 
            end
         end
@@ -61,4 +68,3 @@ end
 flag=1;
 
 end
-
