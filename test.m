eeac23e56b557fcc3532359380742ac837c697e3
unknown <canhan97na@gmail.com>
2020-10-20 20:26:38 +0700

    update

--- test.m
+++ test.m
@@ -2,5 +2,15 @@ clc
 
 
 A = importdata('data.txt');
+for ii = 1:9
+    for jj = 1:9
+        if(A(ii,jj)~=0)
+            if (ConflictTest(A,ii,jj) == 0)
+                disp("Conflict!")
+                return
+            end
+        end
+    end
+end
 A_sol = SudokuBacktracker(A, 1, 1);
 disp(A_sol);
